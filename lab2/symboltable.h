786e9ef78fa7514a33ff1e2c3997bb52a62dec3f
Eric Wen <eericwen@icloud.com>
2017-04-16 22:06:49 +0800

    add basic type symbol

--- lab2/symboltable.h
+++ lab2/symboltable.h
@@ -1,5 +1,25 @@
 #ifndef __SYMBOLTABLE_H__
 #define __SYMBOLTABLE_H__
 
+#include "def.h"
+
+typedef enum SymbolTableItemType {
+	BASIC, ARRAY, STRUCTDEF, STRUCT, FUNCTION
+} SymbolTableItemType;
+
+typedef enum BasicType {
+	INT, FLOAT
+} BasicType;
+
+typedef union SymbolType {
+	BasicType basicType;
+} SymbolType;
+
+typedef struct SymbolTableItem {
+	SymbolTableItemType itemType;
+	SymbolType type;
+	char* name;
+} SymbolTableItem;
+
 #endif
 
