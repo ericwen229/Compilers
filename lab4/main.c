b5ce0caa12cc539a8b0e9744e78589709631ae88
ericwen229 <eericwen@icloud.com>
2017-06-24 10:05:43 +0000

    add file output

--- lab4/main.c
+++ lab4/main.c
@@ -49,14 +49,12 @@ int main(int argc, char* argv[]) {
 	if (gError) {
 		return 0;
 	}
-	//printIRCode(gIRCode, stdout);
-	//printf("==========\n");
-	translateIRCode(gIRCode, stdout);
-	//FILE* out = NULL;
-	//if (argc == 2) out = stdout;
-	//else out = fopen(argv[2], "w");
+	FILE* out = NULL;
+	if (argc == 2) out = stdout;
+	else out = fopen(argv[2], "w");
 	//printIRCode(gIRCode, out);
-	//if (argc == 3) fclose(out);
+	translateIRCode(gIRCode, out);
+	if (argc >= 3) fclose(out);
 	freeIRCode(gIRCode);
 	freeTree(gTree);
 	freeSymbolTable(gSymbolTable);
